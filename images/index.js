fdc99b606a0679684b3ac84006487461d23e3afd
daniel.ancuta@3ss.tv <-5}2nJrR%TxRb}V!>
2021-06-08 13:03:54 +0300

    Updates

---
+++ images/index.js
@@ -0,0 +1,80 @@
+import appeteasers from './appeteasers.jpg';
+import fino_side from './fino-side.jpg';
+import peri_peri_chicken from './peri-peri-chicken.jpg';
+import sharing_platters from './sharing-platters.jpg';
+import dessert from './dessert.jpg';
+import sides from './sides.jpg';
+import cataplana_algarve from './cataplana-algarve.jpg';
+import salads from './salads.jpg';
+import burgers_pitas_wraps from './burgers-pitas-wraps.jpg';
+import logo from './menu_logo.png';
+import start_homepage_one from './start-homepage-one.jpg';
+import bottle from './bottle.png';
+import phone from './phone.png';
+import trophy from './trophy.png';
+import shadow from './shadow.png';
+import about_us from './about-us.jpg';
+import about_us1 from './about-us1.jpg';
+import miha from './miha.jpg';
+import dan from './dan.jpg';
+import quarter_chicken from './quarter-chicken.jpg'
+import red_pepper_dip from './red-pepper-dip.jpg';
+import espetada_carnival  from './espetada-carnival.jpg';
+import hummus_with_per_peri_drizzle  from './hummus-with-per-peri-drizzle.jpg';
+import spicy_mixed_olives from './spicy-mixed-olives.jpg';
+import no_bones_platter  from './no-bones-platter.jpg';
+import chicken_livers_and_portuguese_roll from './chicken-livers-and-portuguese-roll.jpg';
+import half_chicken from './half-chicken.jpg';
+import five_chicken_wings from './5-chicken-wings.jpg';
+import chicken_butterfly from './chicken-butterfly.jpg';
+import whole_chicken from './whole-chicken.jpg';
+import three_chicken_wings from './3-chicken-wings.jpg';
+import full_platter from './full-platter.jpg';
+import ten_wings_in_peri_peri_flavors from './10-wings-in-peri-peri-flavors.jpg';
+import corn_on_the_cob from './corn-on-the-cob.jpg';
+import jumbo_platter from './jumbo-platter.jpg';
+import appeteaser_platter from './appeteaser-platter.jpg';
+import grilled_chicken_tenders from './grilled-chicken-tenders.jpg';
+import all_together_now from './all-together-now.jpg';
+
+
+export {
+    appeteasers,
+    fino_side,
+    peri_peri_chicken,
+    sharing_platters,
+    dessert,
+    sides,
+    cataplana_algarve,
+    salads,
+    burgers_pitas_wraps,
+    logo,
+    start_homepage_one,
+    bottle,
+    phone,
+    trophy,
+    shadow,
+    about_us,
+    about_us1,
+    miha,
+    dan,
+    quarter_chicken,
+    red_pepper_dip,
+    espetada_carnival,
+    hummus_with_per_peri_drizzle,
+    spicy_mixed_olives,
+    no_bones_platter,
+    chicken_livers_and_portuguese_roll,
+    half_chicken,
+    five_chicken_wings,
+    chicken_butterfly,
+    whole_chicken,
+    three_chicken_wings,
+    full_platter,
+    ten_wings_in_peri_peri_flavors,
+    corn_on_the_cob,
+    jumbo_platter,
+    appeteaser_platter,
+    grilled_chicken_tenders,
+    all_together_now
+     };
---
+++ precache-manifest.9e46d4bdca2bef44f361f032fef29031.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bf9b06d4a35ee242ef9",
+    "url": "/your_menu_app/static/css/main.a61fb9d5.chunk.css"
+  },
+  {
+    "revision": "2bf9b06d4a35ee242ef9",
+    "url": "/your_menu_app/static/js/main.00faa5a3.chunk.js"
+  },
+  {
+    "revision": "b7a185161910abad695c",
+    "url": "/your_menu_app/static/js/runtime~main.baa26dc5.js"
+  },
+  {
+    "revision": "f09c0f974768b90ef3ea",
+    "url": "/your_menu_app/static/css/2.ef5be8be.chunk.css"
+  },
+  {
+    "revision": "f09c0f974768b90ef3ea",
+    "url": "/your_menu_app/static/js/2.e112796c.chunk.js"
+  },
+  {
+    "revision": "d84e99cc4982ce86d59867c1f8a80674",
+    "url": "/your_menu_app/static/media/appeteasers.d84e99cc.jpg"
+  },
+  {
+    "revision": "0010267976f48d9ba9279251eff6441d",
+    "url": "/your_menu_app/static/media/fino-side.00102679.jpg"
+  },
+  {
+    "revision": "e5af5ae87619dd8330be38e8389d197f",
+    "url": "/your_menu_app/static/media/peri-peri-chicken.e5af5ae8.jpg"
+  },
+  {
+    "revision": "ee916e8a8d304e89e0deccde33e73978",
+    "url": "/your_menu_app/static/media/sharing-platters.ee916e8a.jpg"
+  },
+  {
+    "revision": "f7075bd5e297f00e72d1d4c29a4c78a4",
+    "url": "/your_menu_app/static/media/dessert.f7075bd5.jpg"
+  },
+  {
+    "revision": "a243626d7defa0325a92d2181d551e1a",
+    "url": "/your_menu_app/static/media/sides.a243626d.jpg"
+  },
+  {
+    "revision": "8be94af1daa6e0fc3cf7689c95891a52",
+    "url": "/your_menu_app/static/media/cataplana-algarve.8be94af1.jpg"
+  },
+  {
+    "revision": "4a3e0744e1cb3b654406b12e6be9bf9d",
+    "url": "/your_menu_app/static/media/salads.4a3e0744.jpg"
+  },
+  {
+    "revision": "84e0b730b1fdb092fc9a7794434786c5",
+    "url": "/your_menu_app/static/media/burgers-pitas-wraps.84e0b730.jpg"
+  },
+  {
+    "revision": "68efe3eff344488deae4331c2230e140",
+    "url": "/your_menu_app/static/media/menu_logo.68efe3ef.png"
+  },
+  {
+    "revision": "4941a57b9a68eecddccaa940dd453be4",
+    "url": "/your_menu_app/static/media/start-homepage-one.4941a57b.jpg"
+  },
+  {
+    "revision": "597ae9d2a2cef7bf679b611d84740454",
+    "url": "/your_menu_app/static/media/bottle.597ae9d2.png"
+  },
+  {
+    "revision": "30a733fa5278a8d1e039253cb57fa991",
+    "url": "/your_menu_app/static/media/trophy.30a733fa.png"
+  },
+  {
+    "revision": "2139101ac21c728da0a15e7a7f0103f1",
+    "url": "/your_menu_app/static/media/about-us.2139101a.jpg"
+  },
+  {
+    "revision": "7dfcdf9c7c8b81dd5b4ac11e388a327d",
+    "url": "/your_menu_app/static/media/about_us1.7dfcdf9c.jpg"
+  },
+  {
+    "revision": "1e290829999cba21be847f2e6d0460d5",
+    "url": "/your_menu_app/static/media/tst_1.1e290829.png"
+  },
+  {
+    "revision": "4373eb8e2103efb59c4692b3dea7daf9",
+    "url": "/your_menu_app/static/media/tst_2.4373eb8e.png"
+  },
+  {
+    "revision": "c9b23b89cb07bcb49b549a0294f0f990",
+    "url": "/your_menu_app/static/media/tst_3.c9b23b89.png"
+  },
+  {
+    "revision": "37b33996db01758d185284e7105986f5",
+    "url": "/your_menu_app/static/media/google.37b33996.png"
+  },
+  {
+    "revision": "0fe1600eecdd965eb98403094664072e",
+    "url": "/your_menu_app/static/media/cell.0fe1600e.png"
+  },
+  {
+    "revision": "a6182f0262b14b34bf5866430aef3014",
+    "url": "/your_menu_app/static/media/line.a6182f02.png"
+  },
+  {
+    "revision": "a2f93b9f7af5e73fce4438edc050127b",
+    "url": "/your_menu_app/static/media/bg_sign_up.a2f93b9f.jpg"
+  },
+  {
+    "revision": "bac063397b777197b5ab0e87cd0090c5",
+    "url": "/your_menu_app/static/media/order_hero.bac06339.png"
+  },
+  {
+    "revision": "98db699c48e78153a96a0572f67d3889",
+    "url": "/your_menu_app/static/media/order_hero_sec.98db699c.png"
+  },
+  {
+    "revision": "5e9e092b723edd0005221bfe43734a87",
+    "url": "/your_menu_app/static/media/ordermenu_hero.5e9e092b.png"
+  },
+  {
+    "revision": "f655633593081e70cca02f89b5d35658",
+    "url": "/your_menu_app/static/media/fb.f6556335.jpg"
+  },
+  {
+    "revision": "38c93d1af3a4bbf08d472b9add20905d",
+    "url": "/your_menu_app/index.html"
+  }
+];
